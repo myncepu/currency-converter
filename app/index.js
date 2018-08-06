e156eb92bc6bc23bb80ffac824ade92adbd4bd2a
Yan Pengqiang <yan2010@live.com>
2018-08-06 18:16:47 +0800

    add '' & '' unit global

--- app/index.js
+++ app/index.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import EStyleSheet from 'react-native-extended-stylesheet'
+import {Dimensions} from 'react-native'
 
 import HomeScreen from './screens/Home'
 
+let {width} = Dimensions.get('window')
 // always call EStyleSheet.build() even if you don't use global variables!
 EStyleSheet.build({
-  $primaryBlue: '#4F6D7A'
+  $primaryBlue: '#4F6D7A',
+  $white: '#fff',
+
+  $rem: width > 340 ? 18 : 16
 })
 
 export default class App extends React.Component {
