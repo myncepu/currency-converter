80a3ff0596942e4def54897a12cb84b8e51b7844
Yan Pengqiang <yan2010@live.com>
2018-08-07 18:29:40 +0800

    add some EStyleSheet color variables to index.js

--- app/index.js
+++ app/index.js
@@ -9,6 +9,9 @@ let {width} = Dimensions.get('window')
 EStyleSheet.build({
   $primaryBlue: '#4F6D7A',
   $white: '#fff',
+  $border: '#E2E2E2',
+  $inputText: '#797979',
+  $lightGray: '#F0F0F0',
 
   $rem: width > 340 ? 18 : 16
 })
