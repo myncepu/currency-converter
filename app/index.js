22869aac3c7b2d5f8223d79053f469184b08dff5
Yan Pengqiang <yan2010@live.com>
2018-08-08 10:41:48 +0800

    add header setting button

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
-import React from 'react'
 import EStyleSheet from 'react-native-extended-stylesheet'
 import {Dimensions} from 'react-native'
+import {createStackNavigator} from 'react-navigation'
 
 import HomeScreen from './screens/Home'
 
@@ -13,13 +13,27 @@ EStyleSheet.build({
   $inputText: '#797979',
   $lightGray: '#D9D9D9',
 
-  $rem: width > 340 ? 18 : 16
+  $rem: width > 340 ? 18 : 16,
+
+  // $outline: 1, // outline all components that are using EStyleSheet
 })
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <HomeScreen />
-    )
-  }
-}
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: HomeScreen
+  },
+}, {
+  initialRouteName: 'Home',
+  /* The header config from HomeScreen is now here */
+  navigationOptions: {
+    headerStyle: {
+      backgroundColor: EStyleSheet.globalVars.$primaryBlue,
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+      fontWeight: 'bold',
+    },
+  },
+})
+
+export default AppNavigator
--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import {View, StatusBar} from 'react-native'
+import {Dimensions, KeyboardAvoidingView, TouchableOpacity, View, StatusBar} from 'react-native'
+import Icon from 'react-native-vector-icons/Ionicons'
 
 import {Container} from '../components/Container'
 import {Logo} from '../components/Logo'
@@ -11,6 +12,8 @@ const TEMP_BASE_CURRENCY = 'USD'
 const TEMP_QUOTE_CURRENCY = 'CNY'
 const TEMP_BASE_PRICE = '100'
 const TEMP_QUOTE_PRICE = '700'
+
+const {width} = Dimensions.get('window')
 
 class HomeScreen extends React.Component {
   constructor(props) {
@@ -25,6 +28,19 @@ class HomeScreen extends React.Component {
         price: TEMP_QUOTE_PRICE,
       }
     }
+  }
+
+  static navigationOptions = {
+    title: 'Home',
+    headerRight: (
+      <TouchableOpacity style={{ paddingVertical: 5, paddingHorizontal: 20 }}>
+        <Icon
+          name="ios-settings"
+          size={30}
+          color="white"
+        />
+      </TouchableOpacity>
+    ),
   }
 
   handleBaseCurrencyChange = (currency) => {
@@ -43,31 +59,37 @@ class HomeScreen extends React.Component {
           translucent={false}
           barStyle="light-content"
         />
-        <Logo />
-        <InputWithButton
-          buttonText={this.state.base.currency}
-          defaultValue={this.state.base.price}
-          onPress={() => null}
-          onChangeText={this.handleBaseCurrencyChange}
-        />
-        <InputWithButton
-          buttonText={this.state.quote.currency}
-          value={this.state.quote.price}
-          editable={false}
-          onPress={() => null}
-        />
-        <LastConverted
-          baseCurrency={this.state.base.currency}
-          quoteCurrency={this.state.quote.currency}
-          conversion={7}
-          date={new Date()}
-        />
-        <View style={{ marginVertical: 20 }}>
-          <ClearButton
-            text="Reverse Currencies"
-            onPress={this.reverseCurrencies}
+        <KeyboardAvoidingView
+          behavior="padding"
+          enabled
+          style={{ flex: 1, width, justifyContent: 'center', alignItems: 'center' }}
+        >
+          <Logo />
+          <InputWithButton
+            buttonText={this.state.base.currency}
+            defaultValue={this.state.base.price}
+            onPress={() => null}
+            onChangeText={this.handleBaseCurrencyChange}
           />
-        </View>
+          <InputWithButton
+            buttonText={this.state.quote.currency}
+            value={this.state.quote.price}
+            editable={false}
+            onPress={() => null}
+          />
+          <LastConverted
+            baseCurrency={this.state.base.currency}
+            quoteCurrency={this.state.quote.currency}
+            conversion={7}
+            date={new Date()}
+          />
+          <View style={{ marginVertical: 20 }}>
+            <ClearButton
+              text="Reverse Currencies"
+              onPress={this.reverseCurrencies}
+            />
+          </View>
+        </KeyboardAvoidingView>
       </Container>
     )
   }
