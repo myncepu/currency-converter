369716df1ea3681848667281a50d9db066afa8f1
Yan Pengqiang <yan2010@live.com>
2018-08-19 22:48:54 +0800

    place the DropdownAlert ABOVE the Navigator

--- app/index.js
+++ app/index.js
@@ -1,14 +1,20 @@
 import React from 'react'
+import { View } from 'react-native'
 import App from './config/routes'
 import { AlertProvider } from './components/Alert'
 import { Provider } from 'react-redux'
+import DropdownAlert from 'react-native-dropdownalert'
 
 import store from './config/store'
+import { DropDownHolder }from './utils/DropDownHolder'
 
 export default () => (
-  <Provider store={store}>
-    <AlertProvider>
-      <App />
-    </AlertProvider>
-  </Provider>
+  <View style={{width: '100%', height: '100%'}}>
+    <Provider store={store}>
+      <AlertProvider>
+        <App />
+      </AlertProvider>
+    </Provider>
+    <DropdownAlert ref={(ref) => DropDownHolder.setDropDown(ref)} />
+  </View>
 )
